d68516c9b495d0dcc5d2f0601a67dbc2a55d789b
Radin Garousi <radingarousi.work@gmail.com>
2026-05-18 20:36:15 +0330

    Create User model and write base admin for this model

--- account/models.py
+++ account/models.py
@@ -7,24 +7,44 @@ from .validators import validate_avatar, validate_poster
 
 
 def user_avatar_path(instance, filename):
-    return f"{instance.username}/avatar_{uuid.uuid4()}.{filename.split('.')[-1]}"
+    return f"{instance.pk}/avatar_{uuid.uuid4()}.{filename.split('.')[-1]}"
 
 def user_poster_path(instance, filename):
-    return f"{instance.username}/poster_{uuid.uuid4()}.{filename.split('.')[-1]}"    
+    return f"{instance.pk}/poster_{uuid.uuid4()}.{filename.split('.')[-1]}"    
 
 class User(AbstractUser):
 
-    class ProfileStatus(models.TextChoices):
+    class AccountStatus(models.TextChoices):
         PENDING = "P", "Pending"
         REJECTED = "R", "Rejected"
         VERIFIED = "V", "Verified"
-    
+
     email = models.EmailField(unique=True)
-    avatar = models.ImageField(upload_to=user_avatar_path, blank=True, null=True, validators=[validate_avatar])
-    poster = models.ImageField(upload_to=user_poster_path, blank=True, null=True, validators=[validate_poster])
-    country = models.CharField(max_length=50, blank=True, null=True)
-    city = models.CharField(max_length=50, blank=True, null=True)
-    bio = models.CharField(max_length=300, blank=True, null=True)
+    avatar = models.ImageField(upload_to=user_avatar_path, blank=True, validators=[validate_avatar])
+    poster = models.ImageField(upload_to=user_poster_path, blank=True, validators=[validate_poster])
+    country = models.CharField(max_length=50, blank=True, default="")
+    city = models.CharField(max_length=50, blank=True, default="")
+    bio = models.CharField(max_length=300, blank=True, default="")
     is_private = models.BooleanField(default=False)
-    profile_status = models.CharField(max_length=1, default=ProfileStatus.VERIFIED, choices=ProfileStatus.choices) # This field for reporting system . report system for next versions
-    updated_at = models.DateTimeField(auto_now=True)
+    account_status = models.CharField(max_length=1, default=AccountStatus.VERIFIED, choices=AccountStatus.choices) # This field for reporting system . report system for next versions
+    last_update = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        
+        self.full_clean()
+        if self.account_status == self.AccountStatus.VERIFIED and self.is_active == False:
+            self.is_active = True
+    
+        elif self.account_status == self.AccountStatus.REJECTED and self.is_active == True:
+            self.is_active = False
+        return super().save(*args, **kwargs)
+
+    # avatar_url and poster_url methods:
+    # To avoid hard coding image paths in templates and to provide a default image
+    # when the user has not uploaded an avatar or poster.
+    # Default images are served from the static files directory.
+    def get_avatar_url(self):
+        return self.avatar.url if self.avatar else "images/default/avatar.webp"
+
+    def get_poster_url(self):
+        return self.poster.url if self.poster else "images/default/poster.webp"
--- account/validators.py
+++ account/validators.py
@@ -3,21 +3,21 @@ from PIL import Image
 
 
 # User Model
-def validate_avatar(image):
+def validate_avatar(image): 
+    
     img = Image.open(image)
-
+    if img.width != img.height:
+            raise ValidationError("تصویر پروفایل باید مربع باشد")
     if img.width < 200 or img.height < 200:
         raise ValidationError("تصویر پروفایل باید حداقل ۲۰۰×۲۰۰باشد")
-
-    elif img.width > 512 or img.height > 512:
-        raise ValidationError("تصویر پروفایل نهایتا میتواند ۵۱۲×۵۱۲ باشد")
+    if img.width > 400 or img.height > 400:
+        raise ValidationError("تصویر پروفایل نهایتا میتواند ۴۰۰×۴۰۰ باشد")
     
 
 def validate_poster(image):
+    
     img = Image.open(image)
-
     if img.width < 1280 or img.height < 720:
         raise ValidationError("تصویر پوستر باید حداقل ۷۲۰×۱۲۸۰باشد")
-    
-    elif img.width > 2560 or img.height > 1440:
-        raise ValidationError("تصویر پوستر میتواد نهایتا ۲۵۶۰×۱۴۴۰ باشد")
+    if img.width > 1920 or img.height > 1080:
+        raise ValidationError("تصویر پوستر میتواد نهایتا ۱۰۸۰×۱۹۲۰ باشد")
