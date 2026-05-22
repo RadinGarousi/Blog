32c3f40366a669ff39879dc257cbbb55810afb0a
Radin Garousi <radingarousi.work@gmail.com>
2026-05-23 01:56:45 +0330

    Foramt user model and improve user model image fields logic

--- account/admin.py
+++ account/admin.py
@@ -3,21 +3,19 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
-
 admin.site.unregister(Group)
 
 User = get_user_model()
+
+
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    ordering = ["-date_joined", "username"]
 
     readonly_fields = ["last_login", "date_joined", "last_update"]
 
-    fieldsets = [
-        *(item for item in UserAdmin.fieldsets if item[0] != "Permissions" and item[0] != "Important dates"),
+    fieldsets = [*(item for item in UserAdmin.fieldsets if item[0] != "Permissions" and item[0] != "Important dates"),
         ("Important dates", {"fields": ("last_login", "date_joined", "last_update")}),
-        ("profile", {"fields": ("bio", "is_private", "account_status", "country", "city", "avatar", "poster")})
-    ]
+        ("profile", {"fields": ("bio", "is_private", "account_status", "country", "city", "avatar", "poster")})]
 
-    add_fieldsets = [
-        (None, {"fields": ("username", "email", "password1", "password2")})
-    ]
+    add_fieldsets = [(None, {"fields": ("username", "email", "password1", "password2")})]
--- account/validators.py
+++ account/validators.py
@@ -1,23 +1,21 @@
-from django.core.exceptions import ValidationError
 from PIL import Image
+from django.core.exceptions import ValidationError
 
 
 # User Model
-def validate_avatar(image): 
-    
-    img = Image.open(image)
-    if img.width != img.height:
+def validate_avatar(image):
+    with Image.open(image) as img:
+        if img.width != img.height:
             raise ValidationError("تصویر پروفایل باید مربع باشد")
-    if img.width < 200 or img.height < 200:
-        raise ValidationError("تصویر پروفایل باید حداقل ۲۰۰×۲۰۰باشد")
-    if img.width > 400 or img.height > 400:
-        raise ValidationError("تصویر پروفایل نهایتا میتواند ۴۰۰×۴۰۰ باشد")
-    
+        if img.width < 200 or img.height < 200:
+            raise ValidationError("تصویر پروفایل باید حداقل ۲۰۰×۲۰۰باشد")
+        if img.width > 400 or img.height > 400:
+            raise ValidationError("تصویر پروفایل نهایتا میتواند ۴۰۰×۴۰۰ باشد")
+
 
 def validate_poster(image):
-    
-    img = Image.open(image)
-    if img.width < 1280 or img.height < 720:
-        raise ValidationError("تصویر پوستر باید حداقل ۷۲۰×۱۲۸۰باشد")
-    if img.width > 1920 or img.height > 1080:
-        raise ValidationError("تصویر پوستر میتواد نهایتا ۱۰۸۰×۱۹۲۰ باشد")
+    with Image.open(image) as img:
+        if img.width < 1280 or img.height < 720:
+            raise ValidationError("تصویر پوستر باید حداقل ۷۲۰×۱۲۸۰باشد")
+        if img.width > 1920 or img.height > 1080:
+            raise ValidationError("تصویر پوستر میتواد نهایتا ۱۰۸۰×۱۹۲۰ باشد")
