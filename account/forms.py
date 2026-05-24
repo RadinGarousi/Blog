a894cce4ab4a07a5f24fb02713fe932d60e924e9
Radin Garousi <radingarousi.work@gmail.com>
2026-05-24 22:29:03 +0330

    Add Persian error messages to login form

--- account/forms.py
+++ account/forms.py
@@ -2,13 +2,18 @@ from django import forms
 
 
 class UserLoginForm(forms.Form):
+
+    ERROR_MESSAGES = {"required": "این فیلد الزامی میباشد.", "max_length": "تعداد کاراکتر وارد شده از حد مجاز بیشتر است."}
+
     login =  forms.CharField(
         max_length=254,
         widget=forms.TextInput(attrs={"placeholder": "ایمیل یا یوزرنیم خود را وارد کنید", "autocomplete": "username"}),
-        label="ایمیل یا یزورنیم"
+        label="ایمیل یا یزورنیم",
+        error_messages=ERROR_MESSAGES
     )
 
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={"placeholder": "رمز عبور", "autocomplete": "current-password"}),
-        label="رمز عبور"
+        label="رمز عبور",
+        error_messages=ERROR_MESSAGES
     )
