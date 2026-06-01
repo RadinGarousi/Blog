0cd5dfcd2fc796c7364765670df7dde0a40335d0
Radin Garousi <radingarousi.work@gmail.com>
2026-06-01 16:42:47 +0330

    Add HomeView and pagination for home page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
+
+from blog.models import Blog
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "blog/home.html")
+        blogs = Blog.objects.filter(status=Blog.BlogStatus.VERIFIED).only("title", "preview_body", "author__username").select_related("author")
+        paginator = Paginator(blogs, 10)
+        page_obj = paginator.get_page(request.GET.get("page"))
+        return render(request, "blog/home.html", {"blogs": page_obj})
 
 
 class ExploreView(View):
